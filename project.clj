357434aad9c610a49ba4f44aa898f7c7f3add356
David Tulig <david.tulig@gmail.com>
2015-02-24 21:49:37 -0600

    Setting the versions for the 0.1.0 release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gcal2org "0.1.0-SNAPSHOT"
+(defproject gcal2org "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
